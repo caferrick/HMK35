89df7dc0e3732f82b6c9d6a0c29917fe5c22c007
caferrick <caferrick@gmail.com>
2020-06-28 10:49:37 -0400

    initial Load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppMaterialModule} from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {ReadQueueService} from './queues/read-queue.service';
+import {NgxChildProcessModule} from 'ngx-childprocess';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    NgxChildProcessModule,
+    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ReadQueueService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
